cd1da5348b55151ad7d1ebe69ac53ad8de20c1f6
tony_vict <rtonyvictar@gmail.com>
2025-08-19 22:27:12 +0800

    Level O added

---
+++ src/main/java/Tommy.java
@@ -0,0 +1,6 @@
+public class Tommy {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Tommy\nWhat can I do for you?\nBye. Hope to see you again soon!" );
+
+    }
+}
