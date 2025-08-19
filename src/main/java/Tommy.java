c6cf42a066c36cc51c978cd577b1c31a047c45e1
tony_vict <rtonyvictar@gmail.com>
2025-08-19 22:53:47 +0800

    Level 1 added

--- src/main/java/Tommy.java
+++ src/main/java/Tommy.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Tommy {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Tommy\nWhat can I do for you?\nBye. Hope to see you again soon!" );
 
-    }
-}
+        System.out.println("Hello! I'm Tommy\nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine(); //reads until the user hits enter
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine(); //scanner takes in the next input
+        }
+                System.out.println("Bye. Hope to see you again soon!");
+            }
+
+        }
+
+
+
+
+
+
+
+
+
