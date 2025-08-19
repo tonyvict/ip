c3030010fcedb75aaefc5cce0e20d131ce39a8d1
tony_vict <rtonyvictar@gmail.com>
2025-08-20 00:20:50 +0800

    Level 2 added

--- src/main/java/Tommy.java
+++ src/main/java/Tommy.java
@@ -1,22 +1,48 @@
 import java.util.Scanner;
 
 public class Tommy {
+
+
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm Tommy\nWhat can I do for you?");
-
         Scanner sc = new Scanner(System.in);
+        String[] words = new String[100];
 
-        String input = sc.nextLine(); //reads until the user hits enter
+        while (true) {
+            String input = sc.nextLine();
 
-        while (!input.equals("bye")) {
-            System.out.println(input);
-            input = sc.nextLine(); //scanner takes in the next input
-        }
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (input.equals("list")) {
+                for (int i = 0; i < words.length; i++) {
+                    if (words[i] != null) {
+                        System.out.printf("%d. %s%n", i + 1, words[i]);
+                    }
+                }
+            } else {
+                for (int i = 0; i < 100; i++) {
+                    if (words[i] == null) {
+                        words[i] = input;
+                        System.out.println("added: " + input);
+                        break;
+                    }
+                }
             }
-
         }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
